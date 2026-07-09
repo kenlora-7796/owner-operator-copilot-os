96e6513fe0d4a7a7321176b5c585184bf109473e
kenlora-7796 <kendra.avery14@gmail.com>
2026-07-08 23:31:26 -0500

    Sprint 6: Live Services Foundation - diagnostics and environment validation

--- lib/container/service-container.ts
+++ lib/container/service-container.ts
@@ -1,9 +1,11 @@
+import { environmentValidator } from "../environment";
 import { httpClient } from "../http";
 import { integrationManager } from "../services/integration-manager";
 
 export class ServiceContainer {
   readonly httpClient = httpClient;
   readonly integrationManager = integrationManager;
+  readonly environmentValidator = environmentValidator;
 }
 
 export const services = new ServiceContainer();
---
+++ lib/environment/environment-validator.ts
@@ -0,0 +1,50 @@
+export interface EnvironmentCheck {
+  name: string;
+  configured: boolean;
+}
+
+export class EnvironmentValidator {
+  check(): EnvironmentCheck[] {
+    return [
+      {
+        name: "NEXT_PUBLIC_GOOGLE_MAPS_API_KEY",
+        configured: Boolean(
+          process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
+        ),
+      },
+      {
+        name: "NEXT_PUBLIC_WEATHER_API_KEY",
+        configured: Boolean(
+          process.env.NEXT_PUBLIC_WEATHER_API_KEY
+        ),
+      },
+      {
+        name: "NEXT_PUBLIC_TRAFFIC_API_KEY",
+        configured: Boolean(
+          process.env.NEXT_PUBLIC_TRAFFIC_API_KEY
+        ),
+      },
+      {
+        name: "NEXT_PUBLIC_FUEL_API_KEY",
+        configured: Boolean(
+          process.env.NEXT_PUBLIC_FUEL_API_KEY
+        ),
+      },
+      {
+        name: "NEXT_PUBLIC_TRUCK_PARKING_API_KEY",
+        configured: Boolean(
+          process.env.NEXT_PUBLIC_TRUCK_PARKING_API_KEY
+        ),
+      },
+      {
+        name: "NEXT_PUBLIC_GEOCODING_API_KEY",
+        configured: Boolean(
+          process.env.NEXT_PUBLIC_GEOCODING_API_KEY
+        ),
+      },
+    ];
+  }
+}
+
+export const environmentValidator =
+  new EnvironmentValidator();
